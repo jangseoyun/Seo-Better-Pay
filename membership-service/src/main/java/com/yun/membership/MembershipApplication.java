08ff786c1413827d7697e7b46786bbdb71a3f9c0
seoyunjang <may-primavera@naver.com>
2024-03-06 23:59:48 +0900

    bug:   멀티모듈 의존성 재설정
    - membership-service setting.gradle 제거하고 root에서 관리
    - common build jar plan 설정
    - build.gradle 재설정
    
    #16

--- membership-service/src/main/java/com/yun/membership/MembershipApplication.java
+++ membership-service/src/main/java/com/yun/membership/MembershipApplication.java
@@ -3,7 +3,7 @@ package com.yun.membership;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.yun.*"})
+@SpringBootApplication(scanBasePackages = {"com.yun.membership", "com.yun.common"})
 public class MembershipApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MembershipApplication.class, args);
