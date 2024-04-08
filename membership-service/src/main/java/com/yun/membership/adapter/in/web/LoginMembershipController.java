28aeb1164249d9617b6dbef0d8c6b08ced2f4206
seoyunjang <may-primavera@naver.com>
2024-04-09 06:40:43 +0900

    refact: 로그인 요청에 관한 @Valid 유효성 검증 처리
    - request단에서 valid 유효성 처리
    - exception
    
    #76

--- membership-service/src/main/java/com/yun/membership/adapter/in/web/LoginMembershipController.java
+++ membership-service/src/main/java/com/yun/membership/adapter/in/web/LoginMembershipController.java
@@ -4,9 +4,11 @@ import com.yun.common.WebAdapter;
 import com.yun.membership.adapter.in.web.model.MembershipResult;
 import com.yun.membership.adapter.in.web.model.request.LoginMembershipRequest;
 import com.yun.membership.application.port.in.LoginMembershipUseCase;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +24,7 @@ public class LoginMembershipController {
     private final LoginMembershipUseCase loginMembershipUseCase;
 
     @PostMapping("/login")
-    public ResponseEntity membershipAccessLogin(@RequestBody LoginMembershipRequest request) {
+    public ResponseEntity membershipAccessLogin(@RequestBody @Valid LoginMembershipRequest request) {
         log.info("request = /membership/login | LoginMembershipRequest: {}", request);
         MembershipResult membershipResult = loginMembershipUseCase.requestLogin(request.toCommand());
         return ResponseEntity.ok().body(membershipResult);
--- membership-service/src/main/java/com/yun/membership/adapter/in/web/model/request/LoginMembershipRequest.java
+++ membership-service/src/main/java/com/yun/membership/adapter/in/web/model/request/LoginMembershipRequest.java
@@ -1,12 +1,21 @@
 package com.yun.membership.adapter.in.web.model.request;
 
 import com.yun.membership.application.port.in.LoginMembershipCommand;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * membershipId를 통해 로그인이 가능하다
  */
 public record LoginMembershipRequest(
+        @NotNull @NotBlank @NotEmpty
+        @Length(min = 5, max = 12, message = "아이디는 5자 ~ 12자 사이를 입력합니다.")
+        @Pattern(regexp = "[a-z\\d]{5,12}", message = "가입된 아이디 형태가 아닙니다.")
         String membershipId,
+        @NotNull @NotBlank @NotEmpty
         String membershipPw
 ) {
     public LoginMembershipCommand toCommand() {
