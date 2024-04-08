9aa60878967aa6feac5c2e9715deb409c7437378
seoyunjang <may-primavera@naver.com>
2024-04-08 23:34:05 +0900

    refact: membership request, command 유효성 검사
    - request 형식 체크
    - command 비즈니스로직전 데이터 유효성 체크
    
    #76

--- membership-service/src/main/java/com/yun/membership/adapter/in/web/RegisterMembershipController.java
+++ membership-service/src/main/java/com/yun/membership/adapter/in/web/RegisterMembershipController.java
@@ -5,9 +5,11 @@ import com.yun.membership.adapter.in.web.model.MembershipResult;
 import com.yun.membership.adapter.in.web.model.request.RegisterMembershipRequest;
 import com.yun.membership.application.port.in.RegisterMembershipUseCase;
 import com.yun.membership.domain.Membership;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +25,10 @@ public class RegisterMembershipController {
     private final RegisterMembershipUseCase registerMembershipUseCase;
 
     @PostMapping("/register")
-    public ResponseEntity<MembershipResult> registerMemberShip(@RequestBody RegisterMembershipRequest registerMembershipRequest) {
+    public ResponseEntity<MembershipResult> registerMemberShip(@RequestBody @Validated RegisterMembershipRequest registerMembershipRequest) {
         log.info("membership register request: {}", registerMembershipRequest);
         Membership membership = registerMembershipUseCase.registerMembership(registerMembershipRequest.toCommand());
         return ResponseEntity.ok().body(MembershipResult.success(membership));
     }
+
 }
--- membership-service/src/main/java/com/yun/membership/adapter/in/web/model/request/RegisterMembershipRequest.java
+++ membership-service/src/main/java/com/yun/membership/adapter/in/web/model/request/RegisterMembershipRequest.java
@@ -1,12 +1,31 @@
 package com.yun.membership.adapter.in.web.model.request;
 
 import com.yun.membership.application.port.in.RegisterMembershipCommand;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record RegisterMembershipRequest(
+        @NotNull
+        @NotBlank
+        @Pattern(regexp = "[a-z\\d]{10,12}",
+                message = "특수문자를 제외한 12자 이내 가능합니다.")
         String membershipId,
+        @NotNull
+        @NotBlank
+        @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,10}",
+                message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 10자의 비밀번호여야 합니다.")
         String membershipPw,
+        @Email
+        @NotBlank
         String membershipEmail,
+        @NotNull
+        @NotBlank
+        @Pattern(regexp = "[ㄱ-ㅎ가-힣]", message = "이름은 한글만 가능합니다.")
         String name,
+        @NotNull
+        @NotBlank
         String address
 ) {
     public RegisterMembershipCommand toCommand() {
