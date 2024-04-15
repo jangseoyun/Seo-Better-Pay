db59e2869907dad5d2d980b8cb00fccf778daca8
seoyunjang <may-primavera@naver.com>
2024-04-16 04:12:48 +0900

    refact: httpclient refactoring restClient update setting

---
+++ common/src/main/java/com/yun/common/anotation/WebAdapter.java
@@ -0,0 +1,16 @@
+package com.yun.common.anotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface WebAdapter {
+    //역할을 표시하는 목적일 뿐 별도의 다른 동작은 하지 않는다
+    @AliasFor(annotation = Component.class)
+    String value() default "";
+}
