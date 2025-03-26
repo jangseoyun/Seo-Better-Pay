a378c7cb0ba7cb82bba6073e794389dc2d7edd7e
seoyunjang <may-primavera@naver.com>
2025-03-26 20:19:55 +0900

    refact: banking service 수정
    - 협업을 위한 일괄 수정

--- banking-service/src/main/java/com/yun/banking/BankingServiceApplication.java
+++ banking-service/src/main/java/com/yun/banking/BankingServiceApplication.java
@@ -1,10 +1,11 @@
 package com.yun.banking;
 
+import com.yun.common.configuration.CommonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
-@ComponentScan(basePackages = {"com.yun.*","com.yun.common", "com.yun.loggingservice"})
+@Import(CommonConfig.class)
 @SpringBootApplication
 public class BankingServiceApplication {
 
